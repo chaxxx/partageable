8815773d57591b72e63ea5b2f701c9bf312e7c91
chaxxx <tamazihanane9@gmail.com>
2016-01-22 16:48:50 +0100

    Modifications dans comp1 et comp4

--- Composant3/Composant3.cpp
+++ Composant3/Composant3.cpp
@@ -2,19 +2,18 @@
 #include "Composant3Version.h"
 
 // declaration des fonctions internes
-int additionCompos3(int a1, int a2);
 
-
-
-int composant3(int p1, int p2)
-{
-	return additionCompos3(p1,p2);
-}
-
-int additionCompos3(int a1, int a2)
+int addition_Composant3(int a1, int a2)
 {
 	return a1+a2;
 }
+
+int composant3(int p1, int p2)
+{
+	return addition_Composant3(p1,p2);
+}
+
+
 
 char * getComposant3Version()
 {
--- Composant4/Composant4.cpp
+++ Composant4/Composant4.cpp
@@ -1,13 +1,10 @@
-
+#include "Composant3.h"
 #include "Composant4.h"
 #include "Composant4Version.h"
-#include "Composant3.h"
  #include <string.h>
 #include <stdlib.h>
 #include <stdio.h> 
 
-// declaration des fonctions internes
-int addition_interne(int a1, int a2);
 
 
 
@@ -16,10 +13,6 @@ int composant4(int p1, int p2)
 	return composant3(p1,p2);
 }
 
-// int addition_interne(int a1, int a2)
-//{
-	//return a1+a2;
-//}
 
 char * getComposant4Version()
 {
