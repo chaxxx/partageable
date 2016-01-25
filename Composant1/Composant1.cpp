76b2b2e34531f7bebbeac09307d673dbc9b971ae
chaxxx <tamazihanane9@gmail.com>
2016-01-25 17:37:51 +0100

    Modifier comp

--- Composant1/Composant1.cpp
+++ Composant1/Composant1.cpp
@@ -6,18 +6,27 @@
 #include <stdlib.h>
 #include <stdio.h> 
 
+// declaration des fonctions internes
+int addition_interne(int a1, int a2);
+
+
+
 int composant1(int p1, int p2)
 {
 	return composant3(p1,p2);
 }
 
+// int addition_interne(int a1, int a2)
+//{
+	//return a1+a2;
+//}
 
 char * getComposant1Version()
 {
 	char * version3=getComposant3Version();
 	char * version1= "Composant 1 version " COMPOSANT_VERSION_STR;
-	char* x = (char*) malloc(strlen(version1)+strlen(version3));
-	 strcpy(x,version1);
-	 strcat(x,version3);
-	return x;
+	char* c = (char*) malloc(strlen(version1)+strlen(version3));
+	 strcpy(c,version1);
+	 strcat(c,version3);
+	return c;
 }
